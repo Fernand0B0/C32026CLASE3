ef39c601f7496002630470bd93238c256e6530fe
Fernand0B0 <Fbogantescr@gmail.com>
2026-01-28 18:25:07 -0600

    Add files via upload

---
+++ TESTINGT/src/main/java/com/mycompany/testingt/TESTINGT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingt;
+
+/**
+ *
+ * @author fboga
+ */
+public class TESTINGT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
