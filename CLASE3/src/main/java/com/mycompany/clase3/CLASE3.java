761bbf4b662c35c948fe7a3bd6a5628d6655b3f9
Fernand0B0 <Fbogantescr@gmail.com>
2026-01-28 20:48:13 -0600

    Add files via upload

---
+++ CLASE3/src/main/java/com/mycompany/clase3/CLASE3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fboga
+ */
+public class CLASE3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        
+        for (int i = 0; i < 10; i++) {
+            JOptionPane.showMessagerDialog(null,"Valor i:" +i);
+            
+        }
+     
+        
+        
+        
+        
+        
+    }
+}
